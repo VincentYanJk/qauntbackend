05e38c58a68f18274a9558bb9a8312128677a09c
jason.yan <jason.yankun@gmail.com>
2025-05-31 19:02:25 +0800

    Remove uncessary example files

--- py_example/train_lstm.py
+++
@@ -1,8 +0,0 @@
-from Quantlib.forecast.trainer.deep_trainer import train_lstm_model
-
-train_lstm_model(
-    csv_path="data/BTCUSDT.csv",
-    save_path="models/lstm_model.pt",
-    seq_len=10,
-    epochs=5
-)
--- py_example/train_xgboost.py
+++
@@ -1,7 +0,0 @@
-from Quantlib.forecast.trainer.trainer import train_model
-
-train_model(
-    df_path="data/BTCUSDT.csv",
-    model_type="xgboost",
-    save_path="models/xgb_model.pkl"
-)
